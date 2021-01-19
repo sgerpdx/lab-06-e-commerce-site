61a099de272e54dfb7745d8b7e43dbf3b0fdb05b
SamuelHGerber <henrylightfoot@gmail.com>
2021-01-19 14:54:56 -0800

    created array with six product objects in equipment.js

--- products/equipment.js
+++ products/equipment.js
@@ -4,7 +4,7 @@
 const medicalEquipment = [
     {
         id: 1001,
-        item: 'hypospray',
+        item: 'Small Hypospray',
         image: 'hypospray-small.png',
         weight: 4,
         size: 'small',
@@ -13,7 +13,82 @@ const medicalEquipment = [
         value: 10,
     }
 
+    {
+        id: 1002,
+        item: 'Large Hypospray',
+        image: 'hypospray-large.png',
+        weight: 6,
+        size: 'large',
+        consumable: true,
+        price: 0,
+        value: 15,
+    }
 
+    {
+        id: 1003,
+        item: 'Medical Tricorder',
+        image: 'hypospray-small.png',
+        weight: 4,
+        size: 'N/A',
+        consumable: false,
+        price: 0,
+        value: 100,
+    }
+
+    {
+        id: 1004,
+        item: 'Wound-Sealing Foam',
+        image: 'hypospray-small.png',
+        weight: 10,
+        size: 'large',
+        consumable: true,
+        price: 0,
+        value: 12,
+    }
+
+    {
+        id: 1004,
+        item: 'Emergency Blanket',
+        image: 'hypospray-small.png',
+        weight: 8,
+        size: 'small',
+        consumable: false,
+        price: 0,
+        value: 9,
+    }
+
+    {
+        id: 1005,
+        item: 'UV Water Purifier',
+        image: 'hypospray-small.png',
+        weight: 7,
+        size: 'small',
+        consumable: false,
+        price: 0,
+        value: 8,
+    }
+
+    {
+        id: 1006,
+        item: 'Cortical Stimulator',
+        image: 'hypospray-small.png',
+        weight: 8,
+        size: 'small',
+        consumable: false,
+        price: 0,
+        value: 80,
+    }
+
+    // {
+    //     id: 1001,
+    //     item: 'hypospray',
+    //     image: 'hypospray-small.png',
+    //     weight: 4,
+    //     size: 'small',
+    //     consumable: true,
+    //     price: 0,
+    //     value: 10,
+    // }
 
 
 
